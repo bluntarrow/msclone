7f3d2a0a33e99abf5114e6d34092ad110455f482
michael <chukwumichael70@gmail.com>
2022-08-17 23:04:10 +0100

    worked half of the app page

--- src/components/app/Toolbar.vue
+++ src/components/app/Toolbar.vue
@@ -1,8 +1,10 @@
 <template>
   <div
-    class="w-full grid grid-cols-5 justify-between  px-4 text-zinc-500 text-sm bg-zinc-900 h-10 items-center"
+    class="w-full grid grid-cols-5 justify-between  px-4 text-zinc-300 text-sm bg-zinc-900 h-10 items-center"
   >
-    <div class="flex items-center">
+    <div class="flex items-center text-zinc-500">
+      <ArrowLeftIcon v-if="$route.name=='app'" class="h-4 mr-5 text-zinc-200"></ArrowLeftIcon>
+
       <div class="h-3 w-3 mr-4 grid grid-cols-2">
         <div class="bg-red-500"></div>
         <div class="bg-green-500"></div>
@@ -15,7 +17,7 @@
       <input
         type="text"
         placeholder="Search Apps, Games, Movies and More"
-        class="w-full max-w-xl py-1 my-1 px-3 rounded-l placeholder:text-zinc-500 bg-zinc-800 outline-none"
+        class="w-full max-w-xl py-1 my-1 px-3 rounded-l placeholder:text-zinc-300 bg-zinc-800 outline-none"
       />
       <div class="my-1 h-full p-2 rounder-r bg-zinc-800">
         <SearchIcon class="h-4"></SearchIcon>
@@ -29,5 +31,5 @@
   </div>
 </template>
 <script setup>
-import {SearchIcon} from '@heroicons/vue/outline'
+import {SearchIcon, ArrowLeftIcon} from '@heroicons/vue/outline'
 </script>
